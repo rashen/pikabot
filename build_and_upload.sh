6d6058b9af8513cf694ba3350b33f1e76e1508e9
Rasmus Henriksson <rasmhenriksson@gmail.com>
2023-12-08 09:52:10 +0100

    Added raspberry pi as target

---
+++ build_and_upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e # Abort at fail
+
+TARGET=arm-unknown-linux-gnueabihf
+TOOLCHAIN=cross-pi-gcc-8.3.0-1
+TARGET_DIR=/home/pi/pikabot/.
+HOST=pi@raspberrypi
+
+PATH=$HOME/toolchains/${TOOLCHAIN}/bin:$PATH \
+cargo build --release --target $TARGET
+
+echo "Transfering binary and dependencies"
+scp -r ./target/$TARGET/release/pikabot ${HOST}:${TARGET_DIR}
+# scp .token ${HOST}:${TARGET_DIR}
+# scp .public_key ${HOST}:${TARGET_DIR}
+# scp .app_id ${HOST}:${TARGET_DIR}
